8f9840efceacb4d227d25e00170525bc11e23827
TothMilan <tothmilan@turr.hu>
2025-04-10 09:38:27 +0200

    test

--- pizza_tm/Form1.cs
+++ pizza_tm/Form1.cs
@@ -3,18 +3,91 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace pizza_tm
 {
     public partial class Form1 : Form
     {
+        public static List <string> p_nev=new List <string> ();
+        public static List <string> p_forint=new List <string> ();
+        public static List <int> p_ar=new List <int> ();
         public Form1()
         {
             InitializeComponent();
+            string FilePath= "pizza.txt";
+            try
+            {
+                using(StreamReader reader=new StreamReader(FilePath))
+                {
+                    string line;
+                    reader.ReadLine();
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] adatok = line.Split('|');
+                        p_nev.Add(adatok[0]);
+                        p_ar.Add(Convert.ToInt32(adatok[1]));
+                        p_forint.Add(adatok[2]);
+
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show (ex.Message);
+            }
+            for(int i = 0; i < p_nev.Count; i++)
+            {
+                comboBox1.Items.Add(p_nev[i] + " " + p_ar[i] + p_forint[i]);
+               
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int plusar = 0;
+            
+            int index = 0;
+          
+            for(int i = 0;i < p_nev.Count; i++)
+            {
+                if (comboBox1.SelectedItem.ToString() == p_nev[i])
+                {
+                    index=i;
+            
+                    break;
+                }
+                /*Szövegkezeléssel. 
+                 * Le kell vágni az ár részét és megkapom a pizza nevét, és utána mehet az index*/
+
+            }
+            plusar = Convert.ToInt32(numericUpDown1.Value) * p_ar[index];
+
+            listBox1.Items.Add(plusar);
+            if (radioButton2.Checked)
+            {
+                plusar += 400;
+            }
+            listBox1.Items.Add(plusar);
+
         }
     }
 }
